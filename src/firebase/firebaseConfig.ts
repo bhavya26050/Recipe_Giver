25bc90d795bec89b84c93a5a7d48daabce286279
Aayushi Rajput <aayushirajput747@gmail.com>
2025-07-07 17:03:53 +0530

    updates

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -5,7 +5,7 @@ import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpp4VZM_hyDxmRsclShMadbn9PrMkVk-E",
-  authDomain: "nutrichef-6dbdd.firebaseapp.com",
+  authDomain: "https://recipe-giver.vercel.app",
   projectId: "nutrichef-6dbdd",
   storageBucket: "nutrichef-6dbdd.firebasestorage.app",
   messagingSenderId: "923897857495",
